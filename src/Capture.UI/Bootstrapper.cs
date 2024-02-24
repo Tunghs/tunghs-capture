48852253affdab9310479401ded1693be5c8ee04
Tunghs <juk08843@gmail.com>
2024-02-24 23:33:42 +0900

    Feat: "개발에 필요한 패키지 설치"

---
+++ src/Capture.UI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Capture.UI
+{
+    public class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            var services = ConfigureServices();
+            Ioc.Default.ConfigureServices(services);
+        }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        /// <returns></returns>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // ViewModel 등록
+            return services.BuildServiceProvider();
+        }
+    }
+}
