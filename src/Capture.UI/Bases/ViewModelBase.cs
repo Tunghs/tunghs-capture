a4121d5f04d1e3e99f26bb1e1d4cdba5df783251
Tunghs <juk08843@gmail.com>
2024-02-25 21:46:03 +0900

    Feat: "ViewModelBase 추가"

---
+++ src/Capture.UI/Bases/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace Capture.UI.Bases
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+        public ViewModelBase() { }
+        public virtual void Cleanup()
+        {
+            WeakReferenceMessenger.Default.Cleanup();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Cleanup();
+            }
+        }
+    }
+}
