aeecf51b4b17918710722c1e501312c6ca89fb79
Tunghs <juk08843@gmail.com>
2024-02-25 21:47:04 +0900

    Feat: "IDialog 추가"

---
+++ src/Capture.UI/Bases/IDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture.UI.Bases
+{
+    public interface IDialog
+    {
+        string Title { get; set; }
+        double Width { get; set; }
+        double Height { get; set; }
+        object DataContext { get; set; }
+        bool Activate();
+
+        void Show();
+
+        bool? ShowDialog();
+
+        void Close();
+
+        Action CloseCallback { get; set; }
+    }
+}
