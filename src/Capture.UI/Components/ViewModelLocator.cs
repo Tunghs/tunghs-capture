ce5e42e2fde41d010cf7823422e29b36e449fc6a
Tunghs <juk08843@gmail.com>
2024-02-29 23:55:20 +0900

    Feat: "ViewModelLocator에 ViewModel 추가"

--- src/Capture.UI/Components/ViewModelLocator.cs
+++ src/Capture.UI/Components/ViewModelLocator.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Capture.UI.ViewModels;
+using CommunityToolkit.Mvvm.DependencyInjection;
 
 namespace Capture.UI.Components
 {
     public sealed class ViewModelLocator
     {
-        
+        public ShellViewModel? ViewModel
+            => Ioc.Default.GetService<ShellViewModel>();
+
+        public CaptureViewModel? CaptureViewModel
+            => Ioc.Default.GetService<CaptureViewModel>();
     }
 }
---
+++ src/Capture.UI/ViewModels/CaptureViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Capture.UI.Bases;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture.UI.ViewModels
+{
+    public class CaptureViewModel : ViewModelBase
+    {
+    }
+}
