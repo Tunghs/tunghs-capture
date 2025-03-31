9a4e30e1d6be81cd77138a2481feec7c6096519a
Tunghs <juk08843@gmail.com>
2025-04-01 00:01:16 +0900

    Feat: 테스트 버튼 추가

--- src/Capture.UI/MainWindow.xaml.cs
+++ src/Capture.UI/MainWindow.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using Capture.UI.Core;
 
-using System.Drawing;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 using Wpf.Ui.Controls;
 
@@ -32,33 +22,43 @@ namespace Capture.UI
             _bitmapConverter = new BitmapConverter();
         }
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void DragBtn_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            var screens = Screen.AllScreens;
-            foreach (var screen in screens)
-            {
-                Console.WriteLine(screen.DeviceName);
-                Console.WriteLine(screen.Bounds.Location);
+           
+            //var screens = Screen.AllScreens;
+            //foreach (var screen in screens)
+            //{
+            //    Console.WriteLine(screen.DeviceName);
+            //    Console.WriteLine(screen.Bounds.Location);
 
-                int Left = screen.Bounds.Left;
-                int top = screen.Bounds.Top;
-                int width = screen.Bounds.Width;
-                int height = screen.Bounds.Height;
+            //    int Left = screen.Bounds.Left;
+            //    int top = screen.Bounds.Top;
+            //    int width = screen.Bounds.Width;
+            //    int height = screen.Bounds.Height;
 
+            //    using (Bitmap sceen = _capture.Capture(Left, top, width, height))
+            //    {
+            //        BitmapImage screenImg = _bitmapConverter.BitmapToBitmapImage(sceen);
 
-                using (Bitmap sceen = _capture.Capture(Left, top, width, height))
-                {
-                    BitmapImage screenImg = _bitmapConverter.BitmapToBitmapImage(sceen);
+            //        ScreenCaptureWindow wd = new ScreenCaptureWindow(screenImg);
+            //        wd.Width = screen.Bounds.Width;
+            //        wd.Height = screen.Bounds.Height;
+            //        wd.Left = screen.Bounds.Left;
+            //        wd.Top = screen.Bounds.Top;
 
-                    ScreenCaptureWindow wd = new ScreenCaptureWindow(screenImg);
-                    wd.Width = screen.Bounds.Width;
-                    wd.Height = screen.Bounds.Height;
-                    wd.Left = screen.Bounds.Left;
-                    wd.Top = screen.Bounds.Top;
+            //        wd.Show();
+            //    }
+            //}
+        }
 
-                    wd.Show();
-                }
-            }
+        private void Button_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
